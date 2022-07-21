eada013eca9a18450c89f0cbc07fcdf0a39ebcf4
Daiana9200 <109746261+Daiana9200@users.noreply.github.com>
2022-07-21 11:50:05 -0300

    Add files via upload
    A ._ N_. A ._ H .... I ..

---
+++ 2107AnahiLedMorce.ino
@@ -0,0 +1,49 @@
+void setup() {
+  // put your setup code here, to run once:
+
+ digitalWrite(LED_BUILTIN, LOW);    
+  delay(200);                      
+  digitalWrite(LED_BUILTIN, HIGH);  //short blink
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(400);                      
+  digitalWrite(LED_BUILTIN, HIGH);  //long blink//largp  //A
+  delay(400); 
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(400);                      
+  digitalWrite(LED_BUILTIN, HIGH);  //long blink//largp
+  delay(400); 
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200);                      
+  digitalWrite(LED_BUILTIN, HIGH);  //short blink //N
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200);                      
+  digitalWrite(LED_BUILTIN, HIGH);  //short blink
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(400);                      
+  digitalWrite(LED_BUILTIN, HIGH);  //long blink//largp //A
+  delay(400); 
+  digitalWrite(LED_BUILTIN, LOW); 
+   digitalWrite(LED_BUILTIN, HIGH);  //short blink
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW); 
+   digitalWrite(LED_BUILTIN, HIGH);  //short blink
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW); 
+   digitalWrite(LED_BUILTIN, HIGH);  //short blink
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW); 
+   digitalWrite(LED_BUILTIN, HIGH);  //short blink //H
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);  //long blink//largp 
+  delay(400); 
+  digitalWrite(LED_BUILTIN, HIGH);  //long blink//largp //I
+  delay(400); 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
